4f7c45408344409e228af4009b03c3cb95d3087b
Sergei-SLS <lyamin.sls@gmail.com>
2024-10-01 20:24:34 +0600

    corrected link API

--- js/const.js
+++ js/const.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://productive-bald-building.glitch.me";
+export const API_URL = "https://productive-bald-building.glitch.me";
 export const PREFIX_PRODUCT = "/api/product";
